15d75dc547cb9977ace8cbd78ca74ea1a8c671ab
ShelbyCohen <s.cohen131@gmail.com>
2019-07-11 18:05:33 -0700

    Adding directories

---
+++ src/main/kotlin/com/music/demo/MusicMapApplication.kt
@@ -0,0 +1,11 @@
+package com.music.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MusicMapApplication
+
+fun main(args: Array<String>) {
+	runApplication<MusicMapApplication>(*args)
+}
